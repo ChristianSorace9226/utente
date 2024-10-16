360c97f84bb7539c720db6a7c278cb9eac6e5ac0
ChristianSorace9226 <soracechristian0@gmail.com>
2024-10-16 12:29:18 +0200

    FIX/ Remove UtenteController.java

--- src/main/java/it/nesea/utente/controller/UtenteController.java
+++
@@ -1,4 +0,0 @@
-package it.nesea.utente.controller;
-
-public class UtenteController {
-}
--- src/main/java/it/nesea/utente/dto/UserDto.java
+++ src/main/java/it/nesea/utente/dto/UserDto.java
@@ -1,9 +1,16 @@
 package it.nesea.utente.dto;
 
 
+import lombok.*;
+
 @Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class UserDto {
 
+    private Long id;
     private String username;
     private String password;
     private String email;
