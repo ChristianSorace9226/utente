26a3329ae9f40d2e83d816517385d5273b6144dc
ChristianSorace9226 <soracechristian0@gmail.com>
2024-10-16 11:38:46 +0200

    FIRST PUSH MASTER

---
+++ src/main/java/it/nesea/utente/model/entity/User.java
@@ -0,0 +1,43 @@
+package it.nesea.utente.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private Boolean enabled = true;
+
+    @Column(nullable = false)
+    private Long roleId;
+
+//    @ManyToMany(fetch = FetchType.EAGER)
+//    @JoinTable(
+//            name = "users_roles",
+//            joinColumns = @JoinColumn(name = "users_id"),
+//            inverseJoinColumns = @JoinColumn(name = "roles_id"))
+//    private Set<Role> roles;
+
+}
---
+++ src/main/java/it/nesea/utente/model/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package it.nesea.utente.model.repository;
+
+import it.nesea.utente.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+}
